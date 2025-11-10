3b86b39be998f27d6e467d19320c60e1eae31441
LeloJonas02 <27807703@sun.ac.za>
2025-11-10 01:45:56 +0000

    commit and push the additional boolean comparisons to the script.js file to the main repository

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let myString = "hello";
 let myBoolean = true;
 let myNumber = 42;
+
+10 > 5; // true; 10 is greater than 5
+3 < 8; // true; 3 is less than 8
+7 === 7; // true; 7 is strictly equal to 7 (same value and type)
+4 !== 9; // true; 4 is not equal to 9 
+6 >= 6; // true; 6 is greater than or equal to 6
